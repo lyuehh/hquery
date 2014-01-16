f902a2e7cf23c8704afa2f9d8bf3496e5078f0af
lyuehh <lyuehh@gmail.com>
2014-01-16 23:27:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# hquery [![Build Status](https://secure.travis-ci.org/lyuehh/hquery.png?branch=master)](http://travis-ci.org/lyuehh/hquery)
+
+jquery for html in command line
+
+## Getting Started
+Install the module with: `npm install hquery`
+
+```javascript
+var hquery = require('hquery');
+hquery.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2014 lyuehh. Licensed under the MIT license.
---
+++ bin/cli.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+var argv = require('optimist').argv;
+var jsdom = require('jsdom');
+var fs = require('fs');
+var path = require('path');
+
+var jquery = fs.readFileSync(path.resolve(path.dirname(__dirname) + '/lib/jquery-2.0.3.js'), 'utf8');
+
+var str = '';
+
+function printUsage() {
+    console.log('Usage: hquery -s "<div id="aa">html string</div>" -e "#aa" -c ".text()" ');
+    console.log(' url -s http://lyuehh.com | hquery -e "h1" -c ".text()"');
+    process.exit(0);
+}
+
+if (!argv.e || !argv.c || argv.h) {
+    printUsage();
+}
+
+if (!argv.s) {
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+
+    process.stdin.on('data', function(chunk) {
+        str += chunk;
+    });
+
+    process.stdin.on('end', function() {
+        run(str);
+    });
+
+    process.stdin.on('error', function(err) {
+        throw err;
+    });
+} else {
+    run(argv.s);
+}
+
+function run(str) {
+    if (!str) {
+        throw 'no string to parse, check your url or argv';
+    }
+    jsdom.env({html: str,  src: [jquery], done: function(err, window) {
+        var $ = window.$;
+        var command = '$("'+ argv.e +'")' + argv.c;
+        console.log(eval(command));
+    }});
+}
