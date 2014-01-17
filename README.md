614e1280531d1486b14aab81b4da20fdccc41aff
lyuehh <lyuehh@gmail.com>
2014-01-17 23:57:57 +0800

    add underscore, rewrite code, update readme

--- README.md
+++ README.md
@@ -1,34 +1,40 @@
 # hquery [![Build Status](https://secure.travis-ci.org/lyuehh/hquery.png?branch=master)](http://travis-ci.org/lyuehh/hquery)
 
-jQuery for html in command line
+Parse HTML with jQuery and Underscore in your commnd line.
 
 ## Getting Started
 Install : `npm install hquery -g`
 
 ## Usage
 
-`hquery -s html_string -e css_selecters -c jquery_methods`<br/>
-or as pipe:
-`curl -s http://lyuehh.com | hquery -e "h1" -c ".text()"`
+```
+Usage: hquery [options] [commands]
+
+Options:
+ -p if you use hquery in pipe
+ -s if you want parse html string
+ -r the javascript code you want to run
+ -f the javascript file you want to run
+```
 
 ## Examples
 
-`$ hquery -s "<div id="aa">html string</div>" -e "#aa" -c ".text()"`
+```
+curl -s https://github.com | hquery -p -r '$("title").html()'
+hquery -s '<div id="aa"><p>haha<p></div>' -r '$("#aa").html()'
+hquery -r '_.range(1, 10)'
+curl -s https://github.com | hquery -p -f a.js
+```
 
--> `html string`
-
-`$ curl -s http://lyuehh.com | hquery -e "h1" -c ".text()"`
-
--> `Hi, welcome.`
-
-You can use any css selecters and almost any jQuery methods to find what you are looking for in the html string.
+You can use any valid JavaScript code with jQuery and Underscore to parse HTML and look for something interesting.
 
 ## Contributing
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
 
-* 2014-01-16 init
+* 2014-01-16 0.0.1 init
+* 2014-01-17 0.0.2 rewrite and add examples
 
 ## License
 Copyright (c) 2014 lyuehh. Licensed under the MIT license.
--- bin/cli.js
+++ bin/cli.js
@@ -6,20 +6,40 @@ var fs = require('fs');
 var path = require('path');
 
 var jquery = fs.readFileSync(path.resolve(path.dirname(__dirname) + '/lib/jquery-2.0.3.js'), 'utf8');
+var underscore = fs.readFileSync(path.resolve(path.dirname(__dirname) + '/lib/underscore-1.5.2.js'), 'utf8');
+
+if (argv.f) {
+    var fileContent = fs.readFileSync('./' + argv.f, 'utf8');
+}
 
 var str = '';
 
 function printUsage() {
-    console.log('Usage: hquery -s "<div id=aa>html string</div>" -e "#aa" -c ".text()" ');
-    console.log(' curl -s http://lyuehh.com | hquery -e "h1" -c ".text()"');
+    console.log('Usage: hquery [options] [commands]');
+    console.log('');
+    console.log('Options: ');
+    console.log(' -p if you use hquery in pipe');
+    console.log(' -s if you want parse html string');
+    console.log(' -r the javascript code you want to run');
+    console.log(' -f the javascript file you want to run');
+    console.log('');
+    console.log('Examples: ');
+    console.log('curl -s https://github.com | hquery -p -r \'$("title").html()\' ');
+    console.log('hquery -s \'<div id="aa"><p>haha<p></div>\' -r \'$("#aa").html()\'');
+    console.log('hquery -r \'_.range(1, 10)\'');
+    console.log('curl -s https://github.com | hquery -p -f a.js');
     process.exit(0);
 }
 
-if (!argv.e || !argv.c || argv.h) {
+if (argv.h && !argv.r) {
     printUsage();
 }
 
-if (!argv.s) {
+if (argv._.length === 0) {
+    printUsage();
+}
+
+if (argv.p) {
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
 
@@ -39,15 +59,25 @@ if (!argv.s) {
 }
 
 function run(str) {
-    if (!str) {
-        throw 'no string to parse, check your url or argv';
+
+    if (argv.p && !str) {
+        throw 'no string to parse, check your url or your command';
     }
-    jsdom.env({html: str,  src: [jquery], done: function(err, window) {
+    if (!argv.p && !argv.s) {
+        str = '<div><div>';
+    }
+    jsdom.env({html: str,  src: [jquery, underscore], done: function(err, window) {
         if (err) {
             throw err;
         }
         var $ = window.$;
-        var command = '$("'+ argv.e +'")' + argv.c;
+        var _ = window._;
+        var command;
+        if (argv.r) {
+            command = argv.r;
+        } else if (argv.f) {
+            command = fileContent;
+        }
         console.log(eval(command));
     }});
 }
